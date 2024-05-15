0773321396d2e334cccddb96f248a8cb36df4f5b
Assaf <asafalt61@gmail.com>
2024-05-15 19:13:22 +0300

    moving function from controller

--- src/main/java/com/capitan/chatapp/services/ChatService.java
+++ src/main/java/com/capitan/chatapp/services/ChatService.java
@@ -1,6 +1,7 @@
 package com.capitan.chatapp.services;
 
 import java.util.Optional;
+import java.util.stream.Collectors;
 import java.util.ArrayList;
 import java.util.LinkedHashMap;
 import java.util.List;
@@ -16,21 +17,73 @@ import org.springframework.stereotype.Service;
 import com.capitan.chatapp.dto.ChatMessageDto;
 import com.capitan.chatapp.models.ChatMessage;
 import com.capitan.chatapp.models.Conversation;
+import com.capitan.chatapp.models.UserEntity;
 import com.capitan.chatapp.repository.ChatRepository;
 import com.capitan.chatapp.repository.ConversationRepository;
+import com.capitan.chatapp.repository.FriendshipRepository;
+import com.capitan.chatapp.repository.UserRepository;
 import com.capitan.chatapp.dto.ChatMessageResponseDto;
+import com.capitan.chatapp.dto.FriendDto;
+import com.capitan.chatapp.dto.FriendUpdateDto;
+import com.capitan.chatapp.dto.FriendUpdateDto.MessageType;
 import com.capitan.chatapp.dto.MessagesHistoryResponseDto;
+
+import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
 
 @Service
 public class ChatService {
 
     private ConversationRepository conversationRepository;
     private ChatRepository chatRepository;
+    private UserRepository userRepository;
+    private FriendshipRepository friendshipRepository;
+    private SimpMessagingTemplate simpMessagingTemplate;
 
     public ChatService(ConversationRepository conversationRepository,
-            ChatRepository chatRepository) {
+            ChatRepository chatRepository, UserRepository userRepository, FriendshipRepository friendshipRepository,
+            SimpMessagingTemplate simpMessagingTemplate) {
         this.conversationRepository = conversationRepository;
         this.chatRepository = chatRepository;
+        this.userRepository = userRepository;
+        this.friendshipRepository = friendshipRepository;
+        this.simpMessagingTemplate = simpMessagingTemplate;
+    }
+
+    public void connectEvent(StompHeaderAccessor headerAccessor) {
+        try {
+            String username = headerAccessor.getUser().getName();
+            Optional<UserEntity> user = userRepository
+                    .findByUsername(username);
+            if (user.isPresent()) {
+                UserEntity opUser = user.get();
+                userRepository.updateOnlineStatus(username, true);
+                String nickname = opUser.getNickname();
+                headerAccessor.setSessionId(nickname);
+
+                // Retrieve the user's online friends
+                Optional<List<FriendDto>> friendsOptional = friendshipRepository.getOnlineFriends(opUser.getId());
+
+                friendsOptional.ifPresent(friends -> {
+                    List<String> onlineFriendNames = friends.stream()
+                            .map(FriendDto::getNickname)
+                            .collect(Collectors.toList());
+
+                    // Notify each friend about the user's online status
+                    onlineFriendNames.forEach(name -> {
+                        FriendUpdateDto messagePayload = new FriendUpdateDto(nickname,
+                                MessageType.JOIN);
+
+                        simpMessagingTemplate.convertAndSendToUser(name, "/queue/onlineFriends", messagePayload);
+                    });
+
+                });
+            }
+        } catch (
+
+        Exception e) {
+            e.printStackTrace();
+        }
     }
 
     public void saveMessage(ChatMessageDto chatMessageDto) {
@@ -94,4 +147,39 @@ public class ChatService {
 
     }
 
+    public void disconnectEvent(StompHeaderAccessor headerAccessor) {
+        try {
+            String username = headerAccessor.getUser().getName();
+            Optional<UserEntity> user = userRepository
+                    .findByUsername(username);
+            if (user.isPresent()) {
+                UserEntity opUser = user.get();
+                userRepository.updateOnlineStatus(username, true);
+                String nickname = opUser.getNickname();
+                headerAccessor.setSessionId(nickname);
+
+                Optional<List<FriendDto>> friendsOptional = friendshipRepository.getOnlineFriends(opUser.getId());
+
+                friendsOptional.ifPresent(friends -> {
+                    List<String> onlineFriendNames = friends.stream()
+                            .map(FriendDto::getNickname)
+                            .collect(Collectors.toList());
+
+                    // Notify each friend about the user's online status
+                    onlineFriendNames.forEach(name -> {
+                        FriendUpdateDto messagePayload = new FriendUpdateDto(nickname,
+                                MessageType.LEAVE);
+
+                        simpMessagingTemplate.convertAndSendToUser(name, "/queue/onlineFriends", messagePayload);
+                    });
+
+                });
+                userRepository.updateOnlineStatus(username, false);
+                System.out.println("Disconnected: " + username);
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
